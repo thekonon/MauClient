a125324cd646762d3b89d5a62a5bdd8c82a06ba4
theKonon <thekonon@gmail.com>
2025-08-05 19:56:43 +0200

    Fixed keeping only last card in pile

--- src/game/pile.ts
+++ src/game/pile.ts
@@ -3,35 +3,45 @@ import { GameSettings } from "../game_settings";
 import { Card } from "./card";
 
 export class Pile extends Container {
-    current_card: Card | null;
-    previous_card: Card | null;
+    private card_queue: Card[] = [];
+    private last_finished_card: Card | null = null;
+    current_card: Card | null = null;
+    previous_card: Card | null = null;
     constructor() {
         super();
-        this.current_card = null;
-        this.previous_card = null;
     }
 
     public play_card(card: Card) {
-        if (this.previous_card != null) {
-            // Remove card that is no longer valid
-            this.removeChild(this.previous_card);
-        } 
-        this.previous_card = this.current_card;
-        if (this.previous_card === null) {
-            card.x = GameSettings.get_deck_top_x();
-            card.y = GameSettings.get_deck_top_y();
-        }
         this.current_card = card;
+
+        // Add card to queue
+        this.card_queue.push(card);
 
         this.current_card.setLocalEndOfAnimation(
             GameSettings.get_deck_top_x() - GameSettings.card_height * 1.1,
             GameSettings.get_deck_top_y() + (GameSettings.card_height + GameSettings.card_width) / 2,
-            Math.PI*4-Math.PI/2);
+            Math.PI * 4 - Math.PI / 2);
         this.addChild(this.current_card);
-        this.current_card.play();
+        this.current_card.play(undefined, undefined, () => {
+            // Remove the previously finished card (if any)
+            if (this.last_finished_card && this.last_finished_card !== card) {
+                this.last_finished_card.parent?.removeChild(this.last_finished_card);
+            }
+
+            // Mark this card as the last finished one
+            this.last_finished_card = card;
+
+            // Remove from queue
+            const index = this.card_queue.indexOf(card);
+            if (index !== -1) {
+                this.card_queue.splice(index, 1);
+            }
+
+            console.log("Card animation finished. Remaining in queue:", this.card_queue.length);
+        });
     }
 
-    public displayNextColor(color: string){
+    public displayNextColor(color: string) {
         // TODO: add text that is displayed next to pile signaling which color is selected with queen
     }
 }
