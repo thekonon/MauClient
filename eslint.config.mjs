afc74222839dfca9bdf412648f2afb0e8f1ed5e7
theKonon <thekonon@gmail.com>
2025-08-17 09:41:29 +0200

    Lint ignore placeholders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ export default tseslint.config(
       ecmaVersion: "latest",
       sourceType: "module",
     },
-    rules: {},
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
   },
 );
