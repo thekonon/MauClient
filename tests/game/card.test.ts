8f8170300bf9ad5f3c9f7a573627d30f0af2095f
theKonon <thekonon@gmail.com>
2025-09-06 14:42:25 +0200

    card tests uncomment

--- tests/game/card.test.ts
+++ tests/game/card.test.ts
@@ -10,8 +10,8 @@ let app: Application;
 
 beforeAll(async () => {
 
-    const canvasWidth = 400 // 414
-    const canvasHeight = 1000 // 896
+    const canvasWidth = 414
+    const canvasHeight = 896
 
     GameSettings.setScreenDimensions(canvasHeight, canvasWidth)
 
@@ -25,17 +25,17 @@ afterAll(async () => {
     app.destroy(true, { children: true, texture: false });
 });
 
-// describe("card.ts assertions tests", () => {
-//     it.each(suits)("creates card with type %s", async (testedType) => {
-//         const card = await Card.create(testedType, "A");
-//         expect(card.type).toBe(testedType);
-//     });
+describe("card.ts assertions tests", () => {
+    it.each(suits)("creates card with type %s", async (testedType) => {
+        const card = await Card.create(testedType, "A");
+        expect(card.type).toBe(testedType);
+    });
 
-//     it.each(values)("creates card with value %s", async (testedValue) => {
-//         const card = await Card.create("C", testedValue);
-//         expect(card.value).toBe(testedValue);
-//     });
-// })
+    it.each(values)("creates card with value %s", async (testedValue) => {
+        const card = await Card.create("C", testedValue);
+        expect(card.value).toBe(testedValue);
+    });
+})
 describe("card.ts unit tests", () => {
     it("test location definition without middle origin", async () => {
         const newX = 100;
@@ -48,8 +48,8 @@ describe("card.ts unit tests", () => {
         expect(card.y).toBe(0)
         expect(card.parent).toBe(app.stage)
 
-        expect(card.end_animation_point_x).toBe(newX)
-        expect(card.end_animation_point_y).toBe(newY)
+        expect(card.end_animation_point_x).toBeCloseTo(newX)
+        expect(card.end_animation_point_y).toBeCloseTo(newY)
 
 
     });
@@ -64,8 +64,8 @@ describe("card.ts unit tests", () => {
         expect(card.y).toBe(0)
         expect(card.parent).toBe(app.stage)
 
-        expect(card.end_animation_point_x).toBe(newX)
-        expect(card.end_animation_point_y).toBe(newY)
+        expect(card.end_animation_point_x).toBeCloseTo(newX, 5)
+        expect(card.end_animation_point_y).toBeCloseTo(newY, 5)
     });
     it("test location definition with middle origin rotated", async () => {
         const newX = 0;
@@ -79,8 +79,8 @@ describe("card.ts unit tests", () => {
         const expectedX = (card.card_sprite.height - card.card_sprite.width) / 2
         const expectedY = -(card.card_sprite.height + card.card_sprite.width) / 2
 
-        expect(card.end_animation_point_x).toBe(expectedX)
-        expect(card.end_animation_point_y).toBe(expectedY)
+        expect(card.end_animation_point_x).toBeCloseTo(expectedX, 5)
+        expect(card.end_animation_point_y).toBeCloseTo(expectedY, 5)
     });
     it("test location definition with middle origin rotated and moved", async () => {
         const newX = 100;
@@ -94,18 +94,18 @@ describe("card.ts unit tests", () => {
         const expectedX = (card.card_sprite.height - card.card_sprite.width) / 2 + newX
         const expectedY = -(card.card_sprite.height + card.card_sprite.width) / 2 + newY
 
-        expect(card.end_animation_point_x).toBe(expectedX)
-        expect(card.end_animation_point_y).toBe(expectedY)
+        expect(card.end_animation_point_x).toBeCloseTo(expectedX)
+        expect(card.end_animation_point_y).toBeCloseTo(expectedY)
 
         await new Promise((res) => setTimeout(res, 5));
 
-        const duration = 0.02
+        const duration = 0.01
         card.play(duration)
         
         await new Promise((res) => setTimeout(res, 1000*duration * 1.05));
 
-        expect(card.x).toBe(expectedX)
-        expect(card.y).toBe(expectedY)
+        expect(card.x).toBeCloseTo(expectedX)
+        expect(card.y).toBeCloseTo(expectedY)
     })
 }
 )
