a868395e60c6393cd2b7fe396d0156aeeb1b1cb3
theKonon <thekonon@gmail.com>
2025-09-06 14:10:12 +0200

    Vitetest configured to run in browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
+/// <reference types="vitest/config" />
 export default defineConfig({
   server: {
     port: 5173,
     open: true,
     host: true,
+  },
+test: {
+    globals: true,
+    browser: {
+      enabled: true,
+      name: "firefox", // or "firefox", "safari" depending on what you have installed
+      headless: false, // 👈 keep browser window open
+    },
+    include: ["tests/**/*.test.ts"]
   },
 });
